f3af0cefdf78e4a45b79d379ea881f34558e8b9e
Fabiano Prospero <fabiano.prospero@gmail.com>
2023-02-27 09:36:50 +0100

    Removed ansible and rds inputs

--- modules/dev/alb.tf
+++ modules/dev/alb.tf
@@ -26,16 +26,6 @@ resource "aws_lb_target_group" "alb-target-group" {
   }
 }
 
-# resource "aws_lb_target_group_attachment" "tg_attachment_fpsouza" {
-#   target_group_arn = aws_lb_target_group.alb-target-group.arn
-#   target_id        = data.aws_instances.fpsouza_dev_ec2.id
-#   port             = 80
-#   depends_on = [
-#     aws_autoscaling_group.asg_dev,
-#     aws_launch_template.fpsouza_lt-dev
-#   ]
-# }
-
 resource "aws_lb_listener" "lb_listener_http" {
   load_balancer_arn = aws_lb.lb_sg.arn
   port              = "80"
